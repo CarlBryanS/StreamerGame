d086a5f60e6ff53a66426a8a405349b74f67b9e6
CarlB <cdsubarita@student.apc.edu.ph>
2021-08-16 10:52:20 +0800

    Reorganized
    Organized some stuff

---
+++ Assets/StreamerBegins/Scripts/GameManagerScripts/GameParameters.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StreamerBegins{
+    public class GameParameters : MonoBehaviour
+    {      
+        public float Energy;
+        public float Health;
+        public int Money;
+        public int Fans;
+        public int Viewers;
+
+        private static GameParameters instance;
+        // Start is called before the first frame update
+        public static GameParameters Instance 
+        { 
+            get { return instance; } 
+        }    
+
+        private void Awake() 
+        { 
+            if (instance != null && instance != this) 
+            { 
+                Destroy(this.gameObject);
+                return;
+            }
+
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } 
+    }
+}
+
